795a3addc34b5f9937423d5f77908f4cd6d36e42
mdsaj <mdsajjadhossain614@gmail.com>
2024-12-30 22:37:29 +0600

    day 1 hashmap

---
+++ src/main/java/com/practicejava/code_challange/Day1/Hashmap.java
@@ -0,0 +1,26 @@
+package com.practicejava.code_challange.Day1;
+
+import java.util.HashMap;
+
+public class Hashmap {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+
+        hashMap.put("abc", 1);
+        hashMap.put("def", 2);
+        hashMap.put("ghi", 3);
+        hashMap.put("jkl", 4);
+        hashMap.put("mno", 5);
+        hashMap.put("pqr", 6);
+        hashMap.put("tuv", 7);
+        hashMap.put("wxyz", 8);
+        System.out.println(hashMap);
+
+        hashMap.remove("tuv");
+        System.out.println(hashMap);
+
+    }
+
+}
